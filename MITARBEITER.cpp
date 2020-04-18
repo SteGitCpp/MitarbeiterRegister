f42d6b9ba5e57a132f4639dcbfda9ce3761d8b23
SteGitCpp <63851270+SteGitCpp@users.noreply.github.com>
2020-04-19 01:09:03 +0200

    class Mitarbeiter: str() und reset()
    getestet

--- MITARBEITER.cpp
+++ MITARBEITER.cpp
@@ -3,9 +3,7 @@
 
 MITARBEITER::MITARBEITER()
 {
-    NName, VName="x";
-    Gebdat.Jahr, Gebdat.Monat, Gebdat.Tag = 0;
-    Urlaubstage = 0;
+    reset();
 }
 
 MITARBEITER::MITARBEITER(std::string nn, std::string vn, DATUM gd, int ut)
@@ -14,5 +12,21 @@ MITARBEITER::MITARBEITER(std::string nn, std::string vn, DATUM gd, int ut)
     VName = vn;
     Gebdat = gd;
     Urlaubstage = ut;
+}
+
+void MITARBEITER::reset()
+{
+    NName = "x";
+    VName="x";
+    Gebdat.Jahr= 0;
+    Gebdat.Monat = 0;
+    Gebdat.Tag = 0;
+    Urlaubstage = 0;
+    Overwrite = true;
+}
+
+std::string MITARBEITER::str()
+{   
+    return NName + ", " + VName + "\n" + Gebdat.str() + "\n" + std::to_string(Urlaubstage); 
 }
 
--- Testing/mitarbeiter_test1.cpp
+++ Testing/mitarbeiter_test1.cpp
@@ -4,12 +4,18 @@
 
 int main()
 {
+    // Konstruktor
     MITARBEITER P1;
     DATUM Gebdat(04, 20, 1889);
     MITARBEITER P2("Otter", "Olaf", Gebdat , 0);
 
     std::cout << P2.NName << std::endl;
     std::cout << P2.Gebdat.str() << std::endl;
+    
+    // str() und reset()
+    std::cout << P2.str() << std::endl;
+    P2.reset();
+    std::cout << P2.str() << std::endl; 
 
     return 0;
 }
